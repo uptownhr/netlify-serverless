6e86660546abd2ed160a6660eeaad32bef7ffc10
james <uptownhr@gmail.com>
2019-06-27 01:13:38 -0700

    testing out serverless functions

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <span>hello test 1234</span>
+  <pre>
+    {{result}}
+  </pre>
 </template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    async asyncData({route}) {
+      console.log('route', route.query, route)
+      const result = await axios.get(`/.netlify/functions/check-name?hostname=${route.query.hostname}`)
+
+      console.log('result.data', result.data)
+
+      return {
+        result: result.data
+      }
+    }
+  }
+</script>
