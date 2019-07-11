dd48a50f40b0eb2ef9f24512039b24dcb9ff162d
james <uptownhr@gmail.com>
2019-07-10 22:26:59 -0700

    added netlify identity login

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,11 +11,16 @@
     <div v-if="result">x
       <pre>{{result}}</pre>
     </div>
+
+    <div id="login">
+      xxxx
+    </div>
   </div>
 </template>
 
 <script>
   import axios from 'axios'
+  const netlifyIdentity = require('netlify-identity-widget')
 
   export default {
     async asyncData({route}) {
@@ -32,11 +37,45 @@
       }
     },
 
+    mounted () {
+      this.identity()
+    },
+
     methods: {
       async handleSubmit (hostname) {
         const result = await axios.get(`/.netlify/functions/check-name?hostname=${hostname}`)
 
         this.result = result.data
+      },
+
+      identity () {
+        netlifyIdentity.init({
+          container: '#login' // defaults to document.body,
+        });
+
+        console.log('iden')
+
+        netlifyIdentity.open(); // open the modal
+        netlifyIdentity.open('login'); // open the modal to the login tab
+        netlifyIdentity.open('signup'); // open the modal to the signup tab
+
+        netlifyIdentity.on('init', user => console.log('init', user));
+        netlifyIdentity.on('login', user => console.log('login', user));
+        netlifyIdentity.on('logout', () => console.log('Logged out'));
+        netlifyIdentity.on('error', err => console.error('Error', err));
+        netlifyIdentity.on('open', () => console.log('Widget opened'));
+        netlifyIdentity.on('close', () => console.log('Widget closed'));
+
+/*// Close the modal
+        netlifyIdentity.close();
+
+// Log out the user
+        netlifyIdentity.logout();
+
+// Access the underlying GoTrue JS client.
+// Note that doing things directly through the GoTrue client brings a risk of getting out of
+// sync between your state and the widget’s state.
+        netlifyIdentity.gotrue;*/
       }
     }
   }
